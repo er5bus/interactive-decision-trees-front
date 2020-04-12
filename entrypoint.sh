1a9ff4f82247d394d6d7c63a97fb7afa2bf86bcb
Rami Sfari <rami2sfari@gmail.com>
2020-04-13 00:34:15 +0100

    TDC-18 update docker files

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,10 @@ echo -e "Current NODE_ENV is $NODE_ENV \n******\n"
 if [[ $NODE_ENV = 'development' ]]; then
   exec npm start
 elif [[ $NODE_ENV = 'production' ]]; then
-  exec npm install -g http-server && npm run build && cd /app/build && hs -p 3000
+  exec npm install -g http-server 
+  exec npm run build
+  exec cd /app/build
+  exec hs -p 3000
 else
   echo -e "Not valid ENV $NODE_ENV\n*******\nAvailable envs [development, production]"
 fi
